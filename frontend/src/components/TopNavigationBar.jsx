1b60269c6bc85d253f314d22b655cf9d63d90a0a
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-10 16:26:54 -0700

    IMPORT- FavBadge and TopicList imported and utilized

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,9 +1,13 @@
 import '../styles/TopNavigationBar.scss'
+import FavBadge from './FavBadge';
+import TopicList from './TopicList';
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList />
+      <FavBadge />
     </div>
   )
 };
