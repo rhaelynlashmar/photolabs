f2854e82c455b53c0014a8fc717c99a3874cfae8
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-10 15:56:07 -0700

    FEATURE- deconstructed topic, added <span> to use title from topic. Removed SampleData

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,15 +1,12 @@
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: 1,
-  slug: "topic-1",
-  label: "Nature",
-};
 
-const TopicListItem = () => {
+const TopicListItem = ({ topic }) => {
+  const { id, slug, title } = topic;
+
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <span>{title}</span>
     </div>
   );
 };
