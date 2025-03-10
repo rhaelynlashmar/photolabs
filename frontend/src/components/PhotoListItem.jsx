ef8829e6378bcb1b56d7a896f037802997650ff4
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-09 19:59:10 -0700

    ALTER- removed username from user object to simplify and changed props being passed from parent to photo for object clarity

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,17 +2,17 @@ import "../styles/PhotoListItem.scss";
 
 
 
-const PhotoListItem = ({ data }) => {
-  const { id, location, urls, user} = data;
+const PhotoListItem = ({ photo }) => {
+  const { id, location, urls, user} = photo;
   const { city, country} = location;
   const { full, regular } = urls;
-  const { username, name, profile } = user;
+  const { name, profile } = user;
 
   return (
     <div className="photoListItem">
       <img src={regular} alt={`Photo by ${name}`} />
       <img src={profile} alt={`${name}'s profile`} />
-      <h2>{name} ({username})</h2>
+      <h2>{name}</h2>
       <p>{city}, {country}</p>
     </div>
   );
