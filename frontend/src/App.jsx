9687f19fd9320b09fa9a02a3a62df70a8de413f4
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-10 14:26:38 -0700

    DELETE- removed sampleData from App and commented out the mapping function, and re-added the photoListItem seperately

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,31 +2,17 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: 1,
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  urls: {
-    full: "/Image-1-Full.jpeg",
-    regular: "/Image-1-Regular.jpeg",
-  },
-  user: {
-    username: "exampleuser",
-    name: "Joe Example",
-    profile: "/profile-1.jpg",
-  },
-};
+
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   
   return (
     <div className="App">
-      {[...Array(3)].map((photo, index) => (
-      <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
-      ))}
+      {/* {[...Array(3)].map((photo, index) => ( */}
+      {/* // <PhotoListItem key={index} photo={sampleDataForPhotoListItem} /> */}
+      {/* ))} */}
+      <PhotoListItem/>
     </div>
   );
 };
