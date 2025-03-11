deaede199997641ebbb722ea1289ddce15b36624
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-10 18:30:34 -0700

    IMPORT- mock data from corresponding files. Passed down as props topics and photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.scss';
 import HomeRoute from './routes/HomeRoute';
-
+import photos from './mocks/photos';
+import topics from './mocks/topics';
 
 
 // Note: Rendering a single component to build components in isolation
@@ -9,7 +10,7 @@ const App = () => {
   
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photos={photos} topics={topics} />
     </div>
   );
 };
