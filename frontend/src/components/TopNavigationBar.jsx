4e45b61f3fd59fc2f5fc02be1ae8bad75cb5b5c7
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-12 14:03:48 -0700

    Feature- added a conditional check to show if there are any favorited photos to report. Added a prop to pass through if condition is true

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,12 +2,14 @@ import '../styles/TopNavigationBar.scss'
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
-const TopNavigation = ({ topics }) => {
+const TopNavigation = ({ topics, favorites }) => {
+  const isFavPhotoExist = favorites && favorites.length > 0;
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics}/>
-      <FavBadge />
+      <TopicList topics={topics} />
+      <FavBadge isFavPhotoExist={isFavPhotoExist} />
     </div>
   )
 };
