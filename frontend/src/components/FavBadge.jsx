acf80753ab0db388fa20dc9fdf07eb8c46248bb2
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-12 14:59:19 -0700

    Feature- added filler to be applied always in TopNavigationBar

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -4,10 +4,10 @@ import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
-    <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+    <div className="fav-badge">
+      <FavIcon displayAlert={isFavPhotoExist} selected={false} isInTopNavigationBar={true} />
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;
