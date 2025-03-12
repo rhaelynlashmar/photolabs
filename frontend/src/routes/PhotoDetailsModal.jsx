a529d7dd953bf171702fd9283ff82bf9eaed56b8
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-12 16:40:22 -0700

    Testing- passes photos as props and console.logged those details

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,7 +1,9 @@
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({ onClose }) => {
+const PhotoDetailsModal = ({ onClose, photo }) => {
+  console.log("photo details", photo);
+  
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
