b5969412fa870e0bb7df1726596071ac9d29a0fa
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-12 14:05:18 -0700

    Feature- added all favourited props to pass down to necessary components (TopNav and PhotoList)

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,12 +1,21 @@
+import React from 'react';
 import PhotoList from '../components/PhotoList';
 import TopNavigation from '../components/TopNavigationBar';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = ({ topics, photos }) => {
+const HomeRoute = ({ topics, photos, favorites, addFavorite, removeFavorite }) => {
   return (
     <div className="home-route">
-      <TopNavigation topics={topics} />
-      <PhotoList photos={photos} />
+      <TopNavigation 
+        topics={topics} 
+        favorites={favorites}
+      />
+      <PhotoList 
+        photos={photos} 
+        favorites={favorites} 
+        addFavorite={addFavorite} 
+        removeFavorite={removeFavorite} 
+      />
     </div>
   );
 };
