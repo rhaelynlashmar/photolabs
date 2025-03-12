697232fa8c29f24f127583d2657924adf1504564
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-12 13:55:07 -0700

    Feature- added useState for favorited photos, with a addFavorite setState function and removeFavorite setState function. Added coordinating props to pass down to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from './routes/HomeRoute';
 import photos from './mocks/photos';
 import topics from './mocks/topics';
 
-
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  
+  const [favorites, setFavorites] = useState([]);
+
+  const addFavorite = (photo) => {
+    setFavorites([...favorites, photo]);
+    console.log("added photo")
+  };
+
+  const removeFavorite = (photoId) => {
+    setFavorites(favorites.filter(photo => photo.id !== photoId));
+  };
+
   return (
     <div className="App">
-      <HomeRoute photos={photos} topics={topics} />
+      <HomeRoute 
+        photos={photos} 
+        topics={topics} 
+        favorites={favorites} 
+        addFavorite={addFavorite} 
+        removeFavorite={removeFavorite} 
+      />
     </div>
   );
 };
