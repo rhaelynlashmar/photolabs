5bfcea6b610e7204824262e0f2457890cc1ab37b
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-13 12:32:13 -0700

    Refactor- Altered props to accept favorites instead of built in "function"

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,8 +1,9 @@
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = ({ favorites }) => {
+  const isFavPhotoExist = favorites && favorites.length > 0;
+
   return (
     <div className="fav-badge">
       <FavIcon displayAlert={isFavPhotoExist} selected={false} isInTopNavigationBar={true} />
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,17 +1,15 @@
-import '../styles/TopNavigationBar.scss'
+import '../styles/TopNavigationBar.scss';
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
 const TopNavigation = ({ topics, favorites }) => {
-  const isFavPhotoExist = favorites && favorites.length > 0;
-
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={topics} />
-      <FavBadge isFavPhotoExist={isFavPhotoExist} />
+      <FavBadge favorites={favorites} />
     </div>
-  )
+  );
 };
 
 export default TopNavigation;
