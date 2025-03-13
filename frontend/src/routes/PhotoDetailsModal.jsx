11383b5a2c216589c1812b4ee0a5d8999671487e
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-12 18:17:42 -0700

    refactor-m deconstructed photo object

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,9 +1,13 @@
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoList from '../components/PhotoList';
 
 const PhotoDetailsModal = ({ onClose, photo }) => {
-  console.log("photo details", photo);
-  
+  const { id, location, urls, user } = photo;
+  const { city, country } = location;
+  const { regular } = urls;
+  const { name, profile } = user;
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
