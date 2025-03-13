8544bf721fd028af30573b7f4561019695c8df3c
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-03-13 01:13:45 -0700

    imported PhotoFavButton, passed props photo, favorites, addFavorite, removeFavorite, onPhotoClick. Added similarPhotos function and passed it through to photoList

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,20 +1,57 @@
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoFavButton from '../components/PhotoFavButton';
 import PhotoList from '../components/PhotoList';
 
-const PhotoDetailsModal = ({ onClose, photo }) => {
+const PhotoDetailsModal = ({ photo, favorites, addFavorite, removeFavorite, onClose, onPhotoClick }) => {
   const { id, location, urls, user } = photo;
   const { city, country } = location;
   const { regular } = urls;
   const { name, profile } = user;
+
+  const similarPhotos = Object.values(photo.similar_photos);
 
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={onClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
+
+      <div className="photo-details-modal__images">
+        <PhotoFavButton 
+          photo={photo}
+          favorites={favorites} 
+          addFavorite={addFavorite} 
+          removeFavorite={removeFavorite} 
+        />
+        <img className="photo-details-modal__image" src={regular} alt={`Image taken in ${city}, ${country}`}></img>
+      </div>
+      
+      
+      <div className="photo-details-modal__header">
+        <div className="photo-details-modal__photographer-details">
+        <img className="photo-details-modal__photographer-profile" src={profile} alt={`${name}'s profile`}/>
+        <div className="photo-details-modal__photographer-info">{name}
+          <p className="photo-details-modal__photographer-location">{city}, {country}</p>
+          </div>
+        </div>
+        </div>
+      
+    
+      
+      <h3 className="photo-details-modal__top-bar">Similar Photos</h3>
+        <div classname="className='photo-details-modal__images">
+        <PhotoList 
+          photos={similarPhotos} 
+          favorites={favorites} 
+          addFavorite={addFavorite} 
+          removeFavorite={removeFavorite} 
+          onPhotoClick={onPhotoClick}
+      />
+      </div>
+
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
